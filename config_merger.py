3a7cdb7be3e78f28b0e29be9098cf8241e224b71
azarz <amaury.zarzelli@gmail.com>
2023-02-22 17:59:05 +0100

    feat: Add fullConfig.json generation

--- config_merger.py
+++ config_merger.py
@@ -1,12 +1,23 @@
 def merge_configs(list_configs):
     config1 = list_configs[0]
     list_configs.pop(0)
-    for apiKey1, layers1 in config1["generalOptions"]["apiKeys"].items():
-        for config2 in list_configs:
-            for apiKey2, layers2 in config2["generalOptions"]["apiKeys"].items():
-                if apiKey1 == apiKey2:
-                    layers1.extend(layers2)
 
+    full_api_keys = {}
+    for api_key in config1["generalOptions"]["apiKeys"].keys():
+        full_api_keys[api_key] = []
+
+    for config in list_configs:
+        for api_key in config["generalOptions"]["apiKeys"].keys():
+            full_api_keys[api_key] = []
+
+    for api_key, layers in config1["generalOptions"]["apiKeys"].items():
+         full_api_keys[api_key].extend(layers)
+
+    for config in list_configs:
+        for api_key, layers in config["generalOptions"]["apiKeys"].items():
+            full_api_keys[api_key].extend(layers)
+
+    config1["generalOptions"]["apiKeys"] = full_api_keys
     for config2 in list_configs:
         config1["layers"].update(config2["layers"])
 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from importlib.resources import contents
 import json
 
 from config_merger import merge_configs
@@ -6,11 +5,16 @@ from requester import getWMSRCapabilities, getWMSVCapabilities, getWMTSCapabilit
 from wms_parser import parseWMS
 from wmts_parser import parseWMTS
 
-def main(key):
+def main(keys):
+    if not isinstance(keys, list):
+        keys = [keys]
     list_configs = [
-        parseWMTS(getWMTSCapabilities(key), key),
-        parseWMS(getWMSRCapabilities(key), key),
-        parseWMS(getWMSVCapabilities(key), key)
+        config for key in keys
+        for config in [
+            parseWMTS(getWMTSCapabilities(key), key),
+            parseWMS(getWMSRCapabilities(key), key),
+            parseWMS(getWMSVCapabilities(key), key)
+        ]
     ]
     list_configs = [config for config in list_configs if isinstance(config, dict)]
     merged_config = merge_configs(list_configs)
@@ -20,8 +24,12 @@ def main(key):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("key")
-    key = parser.parse_args().key
+    parser.add_argument("keys", nargs="+")
+    keys = parser.parse_args().keys
 
-    with open("dist/{}Config.json".format(key), "w", encoding="utf-8") as file:
-        file.writelines(main(key))
+    config_name = keys[0]
+    if len(keys) > 1:
+        config_name = "full"
+
+    with open("dist/{}Config.json".format(config_name), "w", encoding="utf-8") as file:
+        file.writelines(main(keys))
