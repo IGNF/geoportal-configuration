bb598096b3e9d2e3be236484603aae5f61108224
elias75015 <elias.couppe@gmail.com>
2023-08-25 15:00:16 +0200

    Update requester.py avec URL GPF publiques

--- core/requester.py
+++ core/requester.py
@@ -2,7 +2,7 @@ import requests
 import xmltodict
 
 def getWMTSCapabilities(key):
-    url = "https://wmts.geopf.fr/rok4/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities"
+    url = "https://data.geopf.fr/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities"
     response = requests.get(url)
     if response.status_code != 200:
         return False
@@ -11,7 +11,7 @@ def getWMTSCapabilities(key):
     return dict_capabilities["Capabilities"]
 
 def getWMSRCapabilities(key):
-    url = "https://wms-r.geopf.fr/rok4/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
+    url = "https://data.geopf.fr/wms-r/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
     if key == "inspire":
         url = "https://wms-r.geopf.fr/rok4/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
     response = requests.get(url)
@@ -22,7 +22,7 @@ def getWMSRCapabilities(key):
     return dict_capabilities["WMS_Capabilities"]["Capability"]
 
 def getWMSVCapabilities(key):
-    url = "https://wms-v.geopf.fr/geoserver/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
+    url = "https://data.geopf.fr/wms-v/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
     if key == "inspire":
         url = "https://wms-v.geopf.fr/geoserver/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities"
     response = requests.get(url)
