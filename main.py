ef471c6644ba1e31a10d984c4da9f99ba1b08a5e
amaury.zarzelli <amaury.zarzelli@ign.fr>
2024-10-03 16:55:07 +0200

    feat: add referer option for securised private keys

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from core.vectortiles_parser import parseVectorTiles
 from core.wms_parser import parseWMS
 from core.wmts_parser import parseWMTS
 
-def main(keys):
+def main(keys, referer=""):
     if not isinstance(keys, list):
         keys = [keys]
     list_configs = [
         config for key in keys
         for config in [
-            parseWMTS(getWMTSCapabilities(key), key),
-            parseWMS(getWMSRCapabilities(key), key),
-            parseWMS(getWMSVCapabilities(key), key),
+            parseWMTS(getWMTSCapabilities(key, referer), key),
+            parseWMS(getWMSRCapabilities(key, referer), key),
+            parseWMS(getWMSVCapabilities(key, referer), key),
             parseVectorTiles(key)
         ]
     ]
@@ -30,11 +30,13 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("keys", nargs="+")
+    parser.add_argument("--referer", required=False, default="")
     keys = parser.parse_args().keys
+    referer = parser.parse_args().referer
 
     config_name = keys[0]
     if len(keys) > 1:
         config_name = "custom"
 
     with open("dist/{}Config.json".format(config_name), "w", encoding="utf-8") as file:
-        file.writelines(main(keys))
+        file.writelines(main(keys, referer))
