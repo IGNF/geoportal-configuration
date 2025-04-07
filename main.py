4dc1bf7b7a839ef7f6eee994961f3cac64a553d2
hcaillaud <hector.caillaud@ign.fr>
2025-04-07 17:52:19 +0200

    création d un fichier entrée carto dédié.

--- main.py
+++ main.py
@@ -31,22 +31,23 @@ def main(keys, referer=""):
 
     edito = getEdito()
     if edito:
-        merged_config = merge_edito(merged_config, edito)
+        edito_config = merge_edito(merged_config, edito)
     
     # Filtre des couches selon les propriétés des layers
     conditions = {
         "defaultProjection": lambda prop: not any(substring in prop for substring in ["IGNF:LAMB93","EPSG:2154"]),
         "serviceParams": lambda prop: any(substring in prop["id"] for substring in ["WMTS", "WMS", "TMS"]),
     }
-    merged_config["layers"] = {
+    edito_config["layers"] = {
         layerID: layerParams 
-        for layerID, layerParams in merged_config["layers"].items()
+        for layerID, layerParams in edito_config["layers"].items()
         if all(
             prop in layerParams and condition(layerParams[prop])
             for prop, condition in conditions.items()
         )
     }
-
+    with open("dist/entreeCarto.json".format(config_name), "w", encoding="utf-8") as file:
+        file.writelines(json.dumps(edito_config, indent=2, ensure_ascii=False))
 
     return json.dumps(merged_config, indent=2, ensure_ascii=False)
 
