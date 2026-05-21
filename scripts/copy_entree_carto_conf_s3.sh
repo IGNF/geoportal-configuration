0848edcf28ca119c34c07210322ed1d894c24d75
hcaillaud <hector.caillaud@ign.fr>
2026-05-21 11:11:28 +0200

    update script for 2 env

--- scripts/copy_entree_carto_conf_s3.sh
+++ scripts/copy_entree_carto_conf_s3.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# prod
+# S3_PATH="cartes_s3_dev:dev-ign-mut-cartes/entree-carto"
+# qua
+# S3_PATH="cartes_s3_dev:qua-ign-mut-cartes/entree-carto"
+
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-rclone copy "$SCRIPT_DIR/../dist/entreeCarto.json" cartes_s3_dev:dev-ign-mut-cartes/entree-carto
+
+rclone copy "$SCRIPT_DIR/../dist/entreeCarto.json" "$S3_PATH"
